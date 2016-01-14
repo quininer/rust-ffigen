5cd66b950b44d98836265af0485f67c7d559f1e3
quininer <quininer@live.com>
2016-01-14 21:21:41 +0800

    update example

--- examples/ffigen.rs
+++ examples/ffigen.rs
@@ -1,18 +1,19 @@
 extern crate ffigen;
 
-use std::env::args;
+use std::env::{ var, args };
 use ffigen::GenOptions;
 
+const CLANG_INCLUDE_PATH: &'static str = "/usr/lib/clang/3.7.0/include/";
 
 fn main() {
     // ffigen <header> <link>
 
     let mut argv = args().skip(1);
-    let result = GenOptions::new()
-        .arg("-I/usr/lib/clang/3.7.0/include/")
+    let out = GenOptions::new()
+        .arg(&format!("-I{}", var("CLANG_INCLUDE_PATH").unwrap_or(CLANG_INCLUDE_PATH.into())))
         .header(&argv.next().unwrap())
         .link(&argv.next().unwrap())
         .gen();
 
-    println!("{}", String::from_utf8_lossy(&result));
+    println!("{}", String::from_utf8_lossy(&out));
 }
